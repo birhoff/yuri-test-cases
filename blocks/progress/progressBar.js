f212406f94be415230eb315f24f547a04edba668
birhoff <birhoff@gmail.com>
2013-09-05 23:37:56 +0300

    change progress bar to use only one element

--- blocks/progress/progressBar.js
+++ blocks/progress/progressBar.js
@@ -6,8 +6,6 @@
     var ProgressBar = function (element, settings) {
         this._settings = $.extend({}, ProgressBar.defaults, settings);
         this._$progressBar = $(element);
-        this._$indicator = $("<span class='progress-bar__indicator'></span>");
-        this._$label = $("<span class='progress-bar__label'></span>");
         this._value = 0;
 
         this.init();
@@ -17,14 +15,11 @@
         this._$progressBar.css("width", this._settings.width);
         this._$progressBar.addClass("progress-bar");
         this._$progressBar.children().remove();
-        this._$progressBar.append(this._$indicator);
-        this._$progressBar.append(this._$label);
-        this._$label.text(this._value + "%");
+        this._$progressBar.text("0%");
     };
 
     ProgressBar.prototype.value = function () {
-        var newValue = parseInt(arguments[0], 10),
-            aspect = this._$progressBar.width() / 100;
+        var newValue = parseInt(arguments[0], 10);
 
         // get value with empty args
         if (arguments.length < 1) {
@@ -37,12 +32,8 @@
         if (newValue < 0) newValue = 0;
         if (newValue > 100) newValue = 100;
 
-        if (newValue > 0 && !this._$indicator.hasClass("active")) {
-            this._$indicator.addClass("active");
-        }
-
-        this._$label.text(newValue + "%");
-        this._$indicator.css('width', newValue * aspect);
+        this._$progressBar.text(newValue + "%");
+        this._$progressBar.css('background-size', newValue + "% 100%");
         this._value = newValue;
         return newValue;
     };
