f86b4c15249a4262583ef1209e4ec8eb2389b60c
birhoff <birhoff@gmail.com>
2013-09-07 12:35:01 +0300

    change progress bar

--- blocks/progress/progressBar.js
+++ blocks/progress/progressBar.js
@@ -3,43 +3,43 @@
 (function ($) {
     'use strict';
 
-    var ProgressBar = function (element, settings) {
-        this._settings = $.extend({}, ProgressBar.defaults, settings);
+    var ProgressBar = function (element) {
         this._$progressBar = $(element);
+        this._$progress = $("<span class='progress-bar__progress'></span>");
         this._value = 0;
-
         this.init();
     };
 
     ProgressBar.prototype.init = function () {
-        this._$progressBar.css("width", this._settings.width);
+        this._$progress.css('display', 'none');
         this._$progressBar.addClass("progress-bar");
         this._$progressBar.children().remove();
-        this._$progressBar.text("0%");
+        this._$progressBar.append(this._$progress);
+        this._$progressBar.attr('data-value', '0%');
     };
 
     ProgressBar.prototype.value = function () {
-        var newValue = parseInt(arguments[0], 10);
-
+        var newValue = null;
         // get value with empty args
         if (arguments.length < 1) {
             return this._value;
         }
 
+        newValue = parseInt(arguments[0], 10);
+
         if (!newValue) return null;
         if (!isFinite(newValue))return null;
-
         if (newValue < 0) newValue = 0;
         if (newValue > 100) newValue = 100;
+        if (newValue !== 0 && this._$progress.css('display') === 'none') this._$progress.show();
 
-        this._$progressBar.text(newValue + "%");
-        this._$progressBar.css('background-size', newValue + "% 100%");
         this._value = newValue;
-        return newValue;
-    };
+        newValue = newValue + "%";
 
-    ProgressBar.defaults = {
-        width: "100%"
+        this._$progressBar.attr('data-value', newValue);
+        this._$progress.css('width', newValue);
+
+        return this._value;
     };
 
     window.ProgressBar = ProgressBar;
